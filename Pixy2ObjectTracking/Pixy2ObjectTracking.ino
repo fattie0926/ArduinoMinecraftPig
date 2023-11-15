159e3617dcf1d599b1fd4c6a0f7a5635e4e8bdac
fattie0926 <fattie@fattie.io>
2023-11-15 10:00:51 +0800

    developing functions

--- Pixy2ObjectTracking/Pixy2ObjectTracking.ino
+++ Pixy2ObjectTracking/Pixy2ObjectTracking.ino
@@ -3,35 +3,85 @@
 
 Pixy2 pixy;
 
-int mapValue(int x, int in_min, int in_max, int out_min, int out_max) {
-    // 確保x不超出輸入範圍
-    if (x < in_min) {
-        x = in_min;
-    } else if (x > in_max) {
-        x = in_max;
-    }
-    // 計算映射的浮點值
-    float mapped = (float)(x - in_min) * (float)(out_max - out_min) / (float)(in_max - in_min) + out_min;
-    // 將映射的浮點值轉換為整數
-    return (int)(mapped + 0.5); // 加0.5用於四捨五入到最接近的整數
+// Initial Ports
+const int motorLPin = 5;
+const int motorRPin = 6;
+
+// Setup Speed
+const int MAX_SPEED = 255; // 最大速度值
+const int MIN_SPEED = 0;   // 最小速度值
+
+
+// Setup motor and Pixy2
+void setup() {
+  // Setup Serial Port
+  Serial.begin(115200);
+
+  // Pixy2
+  Serial.print("Starting...\n");
+  pixy.init();
+
+  // Motor
+  pinMode(motorLPin, OUTPUT);
+  pinMode(motorRPin, OUTPUT);
 }
 
+
+// Pixy2 Tracking
 int getPositionDifference(int xPosition, int middlePosition = 157) {
     int positionDifference = xPosition - middlePosition;
-    // 假设屏幕宽度是315，因此最小值是0，最大值是315
-    int mappedDifference = mapValue(positionDifference, -middlePosition, middlePosition, -128, 128);
+    int mappedDifference = map(positionDifference, -middlePosition, middlePosition, -128, 128);
     
     return mappedDifference;
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial.print("Starting...\n");
 
-  pixy.init();
+// Motor Control
+void motorControl(int speed, int rotationOffset) {
+    int speedL = speed + rotationOffset;
+    int speedR = speed - rotationOffset;
+
+    // 保证速度值在合理范围内
+    speedL = constrain(speedL, -128, 128);
+    speedR = constrain(speedR, -128, 128);
+
+    // 将速度范围从 -128-128 映射到 PWM 范围 MIN_SPEED-MAX_SPEED
+    speedL = map(speedL, -128, 128, MIN_SPEED, MAX_SPEED);
+    speedR = map(speedR, -128, 128, MIN_SPEED, MAX_SPEED);
+
+    analogWrite(motorLPin, speedL);
+    analogWrite(motorRPin, speedR);
 }
 
-void loop() {
+void motorControlOld(int speedL, int speedR) {
+  const int maxSpeed = 255;
+  const int minSpeed = 0;
+
+  speedL = map(speedL, 0, 100, minSpeed, maxSpeed);
+  speedR = map(speedR, 0, 100, minSpeed, maxSpeed);
+
+  analogWrite(motorLPin, speedL);
+  analogWrite(motorRPin, speedR);
+}
+
+
+// Pig Mode
+void idleMode() {
+    // 闲置模式的逻辑（随机旋转和行走）
+}
+
+void followCarrot(int xPosition) {
+    // 跟随胡萝卜的逻辑
+}
+
+void interactWithHuman(int xPosition) {
+    // 检测到人时的逻辑（随机跟随或旋转并注视）
+}
+
+
+
+// Test Unit
+void testPrintPositionDiff() {
   // 获取并打印检测到的所有物体
   int blockCount = pixy.ccc.getBlocks();
 
@@ -39,17 +89,21 @@ void loop() {
   if (blockCount > 0) {
     // 遍历所有检测到的物体
     for (int i = 0; i < blockCount; i++) {
-      // 获取物体的x坐标
       int xPosition = pixy.ccc.blocks[i].m_x;
-      //int middlePosition = 157;
-      //int positionDifference = xPosition - middlePosition;
 
-      Serial.print("物体");
+      Serial.print("物體");
       Serial.print(i + 1);
-      Serial.print(": X轴位置 = ");
+      Serial.print(": X Position = ");
       Serial.println(xPosition); 
-      //Serial.println(positionDifference);
+
       Serial.println(getPositionDifference(xPosition));
     }
   }
+}
+
+
+// Main
+void loop() {
+
+
 }
